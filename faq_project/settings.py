2c2c18b43ca2ba288db5fe397c49d47bfb8199f8
mith10 <mithilesh.rajput21@pccoepune.org>
2025-02-02 15:33:04 +0530

    All basic files structure add, moving towards redis

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -121,3 +121,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height': 300,
+        'width': '100%',
+    },
+}
+
